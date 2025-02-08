7906ce16e4003c79ba6ef2b484f1a55220c3d837
John Koehl <jkoehl@gmail.com>
2025-02-08 18:08:19 -0500

    Updated to draw to panel

--- data/modules/main.lua
+++ data/modules/main.lua
@@ -2,19 +2,34 @@ panel2d = false
 panelWidth3d = 2048
 panelHeight3d = 2048
 
+sasl.options.setAircraftPanelRendering(true)
+sasl.options.set3DRendering(false)
+sasl.options.setInteractivity(false)
+
 size = { 2048, 2048 }
 
-newWindow = contextWindow { 
-    name = ’Window’;
-    position = { 50, 50, 640, 640 };
-    noBackground = false; 
-    minimumSize = { 300, 300 };
-    maximumSize = { 1200, 1200 };
-    gravity = { 0, 1, 0, 1 };
-    visible = true;
-    components = { 
-        instrument {
-            position = { 0, 0, 400, 500 };
-        }
+components = { 
+    instrument {
+        position = { 0, 775, 400, 500 };
     }
 }
+
+-- newWindow = contextWindow { 
+--     name = ’Window’;
+--     position = { 50, 50, 640, 640 };
+--     noBackground = false; 
+--     minimumSize = { 300, 300 };
+--     maximumSize = { 1200, 1200 };
+--     gravity = { 0, 1, 0, 1 };
+--     visible = true;
+--     components = { 
+--         instrument {
+--             position = { 0, 0, 400, 500 };
+--         }
+--     }
+-- }
+
+-- Render to texture at
+-- X=0, Y=775
+-- width=400
+-- height=500
